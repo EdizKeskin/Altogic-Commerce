536c8bb67d4f68e9943626dd59a14dfc29d4dccb
Ediz Keskin <edizkeskin@gmail.com>
2023-01-14 21:56:22 +0300

    Search bar added

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,18 +6,22 @@ import {
   MenuItem,
   MenuList,
   Button,
+  Input,
+  InputLeftElement,
+  InputGroup,
 } from "@chakra-ui/react";
 import Header from "../components/header";
 
 import CustomSpinner from "../components/spinner";
 import ProductCard from "../components/productCard";
 import Footer from "../components/footer";
-import React from "react";
-import { AiOutlineDown } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineDown, AiOutlineSearch } from "react-icons/ai";
 import { useLang } from "../context/langContext";
 
 function Home({ products, sort, setSort, setSortType, sortType }) {
   const { lang } = useLang();
+  const [searchText, setSearchText] = useState("");
 
   if (products === null) return <CustomSpinner />;
 
@@ -27,12 +31,38 @@ function Home({ products, sort, setSort, setSortType, sortType }) {
     lang === "tr-TR" ? "Fiyata göre artan" : "Increasing by price";
   const newestProducts = lang === "tr-TR" ? "Yeni Ürünler" : "Newest Products";
 
+  const handleChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const filteredProducts = products.filter((data) => {
+    return (
+      data.title.toLowerCase().includes(searchText.toLowerCase()) ||
+      data.categories.some((category) =>
+        category
+          .toLowerCase()
+          .includes(searchText.toLowerCase(searchText.toLowerCase()))
+      )
+    );
+  });
+
   return (
     <Box minh="100vh">
       <div data-aos="fade-up">
         <Header />
       </div>
       <Box display={"flex"} justifyContent={"flex-end"} px={15}>
+        <InputGroup w={"fit-content"} data-aos="zoom-in-up" mr={3}>
+          <InputLeftElement pointerEvents="none">
+            <AiOutlineSearch />
+          </InputLeftElement>
+          <Input
+            placeholder="Search..."
+            bg={"gray.700"}
+            value={searchText}
+            onChange={handleChange}
+          />
+        </InputGroup>
         <Menu>
           <MenuButton
             as={Button}
@@ -82,8 +112,8 @@ function Home({ products, sort, setSort, setSortType, sortType }) {
         gap={6}
         mt={"4"}
       >
-        {products.map((data, index) => (
-          <div key={index} data-aos="zoom-in-up">
+        {filteredProducts.map((data, i) => (
+          <div key={i} data-aos="zoom-in-up">
             <ProductCard item={data} />
           </div>
         ))}
